e646501ad925e9b7cdd1074655c2d8123f52c908
Yauheniya Sirotkina <zsirotkina@gmail.com>
2021-10-02 16:59:30 +0300

    add AuthorsProfil

---
+++ Library/src/Library.API/Profiles/AuthorsProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Library.API.Helpers;
+
+namespace Library.API.Profiles
+{
+    public class AuthorsProfile : Profile
+    {
+        public AuthorsProfile()
+        {
+            CreateMap<Entities.Author, Models.AuthorDto>()
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(
+                    dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge(src.DateOfDeath)));
+        }
+    }
+}
