c9abc7cd031c153146fa1e5f40d3d481686a898e
Yauheniya Sirotkina <zsirotkina@gmail.com>
2021-10-02 16:48:12 +0300

    add GetCurrentAge extensions

---
+++ Library/src/Library.API/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Library.API.Helpers
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static int GetCurrentAge(this DateTimeOffset dateTimeOffset, DateTimeOffset dateOfDeath)
+        {
+            var currentDate = dateOfDeath != DateTimeOffset.MinValue ? dateOfDeath : DateTime.UtcNow;
+            var age = currentDate.Year - dateTimeOffset.Year;
+
+            if (currentDate < dateTimeOffset.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
